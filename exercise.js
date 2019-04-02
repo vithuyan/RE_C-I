5f032c50584ce33130850e48748e66b825ceef7e
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-04-02 14:33:05 -0400

    done

---
+++ exercise.js
@@ -0,0 +1,5 @@
+const tasks = ["laundry", "shovel driveway", "grocery shopping", "check email", "daydream", "phone parents", "fill prescription", "exercise", "take out recycling", "check the weather"];
+for(let i = 1; i < tasks.length + 1; i++) {
+  console.log("Sunday To Do List:");
+  console.log(i + ':' + task [i - 1]);
+}
